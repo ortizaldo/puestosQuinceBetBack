5dcabbd921991a11f5aba65ae3920db0e90da09a
Aldo Ortiz <ortizaldo14@gmail.com>
2026-09-08 09:40:49 -0600

    feat: update event schema to include additional fields and improve structure

--- src/schemas/Events.js
+++ src/schemas/Events.js
@@ -4,15 +4,91 @@ import { autoIncrement } from "mongoose-plugin-autoinc";
 
 const schema = new mongoose.Schema(
   {
-    nombre: { type: String, required: true },
-    arma: { type: String, required: true },
-    numGallos: { type: Number, required: true },
-    pesos: { type: String, required: true },
-    puntos: { type: String, required: true },
-    fechaEvento: { type: String, required: true },
-    horarioBascula: { type: String, required: true },
+    nombre: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    tipoEvento: {
+      type: String,
+      required: true,
+    },
+
+    arma: {
+      type: String,
+      enum: ["1/4", "1", "1/2"],
+      required: true,
+    },
+
+    numGallos: {
+      type: Number,
+      required: true,
+    },
+
+    creditos: {
+      type: Number,
+      required: true,
+    },
+
+    peleaXDentro: {
+      type: Number,
+      required: false,
+    },
+
+    fechaEvento: {
+      type: Date,
+      required: true,
+    },
+
+    horarioBasculaInicio: {
+      type: String,
+      default: null,
+    },
+
+    horarioBasculaFin: {
+      type: String,
+      default: null,
+    },
+
+    pesoMinimo: { type: Number, required: true },
+    pesoMaximo: { type: Number, required: true },
+
+    flyer: {
+      url: {
+        type: String,
+        default: null,
+      },
+
+      publicId: {
+        type: String,
+        default: null,
+      },
+
+      nombreArchivo: {
+        type: String,
+        default: null,
+      },
+    },
+
+    status: {
+      type: String,
+      enum: [
+        "DRAFT",
+        "PUBLISHED",
+        "OPEN",
+        "IN_PROGRESS",
+        "FINISHED",
+        "CANCELLED",
+      ],
+      default: "DRAFT",
+    },
   },
-  { toObject: { virtuals: true }, toJSON: { virtuals: true }, timestamps: true }
+  {
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+    timestamps: true,
+  },
 );
 
 schema.add(AdminFields);
